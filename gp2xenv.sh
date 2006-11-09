f36e700a8088193e409d10de163c5ab8a3994b04
rixed <rixed>
2006-11-09 15:21:39 +0000

    Removed unused -g flag ;

--- gp2xenv.sh
+++ gp2xenv.sh
@@ -1,5 +1,5 @@
 export GP2XDEVDIR=/usr/local/devkitPro/devkitGP2X
-export CFLAGS="-O3 -g -DNDEBUG -I$GP2XDEVDIR/include -I/usr/local/include"
+export CFLAGS="-O3 -DNDEBUG -I$GP2XDEVDIR/include -I/usr/local/include"
 export LDFLAGS="-L$GP2XDEVDIR/lib" 
 export CC=$GP2XDEVDIR/bin/arm-linux-gcc
 export LD=$GP2XDEVDIR/bin/arm-linux-ld
