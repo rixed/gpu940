cfffc71228ce132db26e280914f07423b42a0297
rixed <rixed>
2007-01-02 22:31:59 +0000

    *** empty log message ***

--- gp2xenv.sh
+++ gp2xenv.sh
@@ -1,5 +1,15 @@
-export GP2XDEVDIR=/usr/local/devkitPro/devkitGP2X
-export CFLAGS="-O3 -DNDEBUG -DGP2X -I$GP2XDEVDIR/include -I/usr/local/include"
+## For official SDK
+#export GP2XDEVDIR=/gp2xsdk/Tools
+#export CC=$GP2XDEVDIR/bin/arm-gp2x-linux-gcc
+#export LD=$GP2XDEVDIR/bin/arm-gp2x-linux-ld
+## For devkitGP2X
+#export GP2XDEVDIR=/usr/local/devkitPro/devkitGP2X
+#export CC=$GP2XDEVDIR/bin/arm-linux-gcc
+#export LD=$GP2XDEVDIR/bin/arm-linux-ld
+## For gp2xsdk
+export GP2XDEVDIR=/usr/local/gp2xdev
+export CC=$GP2XDEVDIR/bin/gp2x-gcc
+export LD=$GP2XDEVDIR/bin/gp2x-ld
+
+export CFLAGS="-O3 -DNDEBUG -DGP2X -I$GP2XDEVDIR/include -I/usr/local/include -mcpu=arm940t -msoft-float"
 export LDFLAGS="-L$GP2XDEVDIR/lib" 
-export CC=$GP2XDEVDIR/bin/arm-linux-gcc
-export LD=$GP2XDEVDIR/bin/arm-linux-ld
