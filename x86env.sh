850b8946fae20b42510120eeec0bd347a7d3608c
rixed <rixed>
2007-02-05 23:37:59 +0000

    Minor build system fixes ;

--- x86env.sh
+++ x86env.sh
@@ -1,8 +1,8 @@
 # We still need to know where the cross compilator is for make dumpcodegen
 . gp2xenv.sh
 # Then override for x86 target
-export CPPFLAGS="-I/usr/local/include"
-export CFLAGS="-O0 -g2 -DSOFT_DIVS=1"
+export CPPFLAGS=""
+export CFLAGS="-O2 -g2 -DSOFT_DIVS=1"
 export LDFLAGS=""
 export CC=colorgcc
 unset LD
